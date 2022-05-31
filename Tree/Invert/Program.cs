f5e4efbbb1e7f7f9c22495fb3d44e26456809f65
Varun <vermavarun@outlook.com>
2022-05-31 16:43:52 +0530

    Invert tree

---
+++ Tree/Invert/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Invert
+{
+    class Node {
+        public Node left;
+        public Node right;
+        public int _data;
+        public Node(int data) {
+            this._data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node root = CreateTree();
+            root = InvertTree(root);
+            PrintLevelOrderTraversal(root);
+        }
+
+        public static Node InvertTree(Node node) {
+
+            if (node == null) {
+                return node;
+            }
+
+            Node left = InvertTree(node.left);
+            Node right = InvertTree(node.right);
+
+            node.left = right;
+            node.right = left;
+
+            return node;
+        }
+        /* Just to create and display tree*/
+        public static Node CreateTree() {
+
+            Node root = new Node(0);
+            Node firstLeft = new Node(1);
+            Node firstRight = new Node(2);
+            Node secondLeft = new Node(3);
+
+            root.left = firstLeft;
+            firstLeft.left = secondLeft;
+            root.right = firstRight;
+
+            return root;
+        }
+
+        public static void PrintLevelOrderTraversal(Node root) {
+            int height = CalculateTreeHeight(root);
+            for (int i=1; i<=height; i++) {
+                PrintCurrentLevel(root, i);
+            }
+
+        }
+
+        public static void PrintCurrentLevel(Node root, int level) {
+
+            if (root == null) {
+                return;
+            }
+
+            if (level == 1) {
+                Console.WriteLine(root._data + " ");
+            }
+            else if (level > 1) {
+                PrintCurrentLevel(root.left, level-1);
+                PrintCurrentLevel(root.right, level-1);
+            }
+        }
+
+        public static int CalculateTreeHeight(Node root) {
+            if(root == null) {
+                return 0;
+            }
+
+            int lHeight = CalculateTreeHeight(root.left);
+            int rHeight = CalculateTreeHeight(root.right);
+
+            return lHeight > rHeight ? lHeight + 1 : rHeight + 1;
+        }
+    }
+}
