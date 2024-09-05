05d088c2dd18e7f0097e4599b68adcd4ff8ae099
Varun Verma <vermavarun@outlook.com>
2024-09-05 12:39:30 +0530

    feat: Implement WordPattern algorithm solution

---
+++ LeetCode-Solutions/0290.WordPattern.cs
@@ -0,0 +1,50 @@
+/*
+
+Approach:
+1. Create two dictionaries, one to store the mapping of word to pattern and another to store the mapping of pattern to word.
+2. Split the string s into words and convert the pattern into a char array.
+3. Iterate through the pattern and for each pattern, check if the word is already mapped to the pattern and if the pattern is already mapped to the word.
+4. If the mapping is not present, add the mapping to the dictionaries.
+5. If the mapping is present, check if the mapping is correct.
+6. If the mapping is not correct, return false.
+7. If the loop completes, return true.
+
+Time complexity: O(n)
+Space complexity: O(n)
+
+
+*/
+public class Solution {
+    public bool WordPattern(string pattern, string s) {
+            Dictionary<string,char> dictWordToPattern = new Dictionary<string, char>();
+            Dictionary<char,string> dictPatternToWord = new Dictionary<char, string>();
+
+            var words = s.Split(' ');
+            var patternChars = pattern.ToCharArray();
+
+            if (words.Length != patternChars.Length) return false;
+
+            int wordsPointer = 0;
+            int patternPointer = 0;
+
+            while(patternPointer < pattern.Length) {
+
+                var currentPattern = pattern[patternPointer];
+                var currentWord = words[wordsPointer];
+
+                if (dictPatternToWord.ContainsKey(currentPattern) && dictPatternToWord[currentPattern] != currentWord) return false;
+
+                if (dictWordToPattern.ContainsKey(currentWord) && dictWordToPattern[currentWord] != currentPattern) return false;
+
+                dictPatternToWord[currentPattern] = currentWord;
+                dictWordToPattern[currentWord] = currentPattern;
+
+
+                wordsPointer++;
+                patternPointer++;
+
+            }
+
+            return true;
+    }
+}
--- LeetCode-Solutions/1189-MaximumNumberOfBalloons.cs
+++ LeetCode-Solutions/1189-MaximumNumberOfBalloons.cs
@@ -55,3 +55,27 @@ public class Solution {
             return count.Min();
     }
 }
+
+/*
+Same approach smaller code
+*/
+
+public class Solution {
+    public int MaxNumberOfBalloons(string text) {
+        var charCounts = text.GroupBy(c => c)
+                        .ToDictionary(g => g.Key, g => g.Count());
+        var balloonCounts = "balloon".GroupBy(c => c)
+                        .ToDictionary(g => g.Key, g => g.Count());
+
+        int result = text.Length;
+        foreach (var balloonCount in balloonCounts) {
+            if (charCounts.ContainsKey(balloonCount.Key)) {
+                result = Math.Min(result, charCounts[balloonCount.Key] / balloonCount.Value);
+            } else {
+                result = 0;
+                break;
+            }
+        }
+        return result;
+    }
+}
