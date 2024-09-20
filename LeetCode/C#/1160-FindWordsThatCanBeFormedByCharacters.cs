7c368e399338cd332b16260d375a4894f8f9fbad
Varun Verma <vermavarun@outlook.com>
2024-09-20 16:04:29 +0530

    feat: Implement solution for counting characters in words
    This commit adds a new solution for counting characters in words. It includes a class `Solution` with a method `CountCharacters` that takes an array of words and a string of characters as input. The method uses dictionaries to store the count of characters in the characters string and in each word. It then checks if a word can be formed from the characters string and adds the length of the word to the result. The time complexity of the solution is O(n*m), where n is the length of the words array and m is the length of the characters string. The space complexity is O(n), where n is the length of the characters string.

---
+++ LeetCode/C#/1160-FindWordsThatCanBeFormedByCharacters.cs
@@ -0,0 +1,56 @@
+/*
+
+Approach:
+1. Create a dictionary of characters and their count in chars string.
+2. For each word in words, create a dictionary of characters and their count.
+3. Check if the word can be formed from chars string.
+    a. If yes, add the length of the word to result.
+4. Return the result.
+
+Time Complexity: O(n*m) where n is the length of words and m is the length of chars.
+Space Complexity: O(n) where n is the length of chars.
+
+*/
+public class Solution {
+    public int CountCharacters(string[] words, string chars) {
+        Dictionary<char,int> dict = new Dictionary<char,int>();
+        Dictionary<char,int> temp = new Dictionary<char,int>();
+        int result = 0;
+        foreach(char ch in chars) {
+            if(!dict.ContainsKey(ch)) {
+                dict[ch] = 1;
+            }
+            else {
+                dict[ch] = dict[ch] + 1;
+            }
+        }
+
+        foreach(string word in words) {
+
+            foreach(char ch in word) {
+               if(!temp.ContainsKey(ch)) {
+                    temp[ch] = 1;
+                }
+                else {
+                    temp[ch] = temp[ch] + 1;
+                }
+            }
+
+            int index = 0;
+            foreach(var kv in temp) {
+                if (dict.ContainsKey(kv.Key) && dict[kv.Key] >= temp[kv.Key])  {
+                    index++;
+                    continue;
+                }
+                else {
+                    break;
+                }
+            }
+            if (index == temp.Count())
+                result = result + word.Length;
+            temp.Clear();
+        }
+        return result;
+
+    }
+}
