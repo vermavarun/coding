81457f97bffc1f6b863a69870e1e22feade5dd44
Varun Verma <vermavarun@outlook.com>
2024-10-09 00:39:47 +0530

    Implement timeLimit function to introduce a time limit for a given function

---
+++ LeetCode/JavaScript/2637-PromiseTimeLimit.js
@@ -0,0 +1,45 @@
+/*
+
+Approach:
+1) We can create a function that returns a function that resolves a promise after a certain amount of time.
+2) We can use the setTimeout function to achieve this.
+3) We can use the async and await keywords to make the function asynchronous.
+4) We can use the Promise constructor to create a promise that resolves after a certain amount of time.
+
+Time complexity:
+- O(1)
+
+Space complexity:
+- O(1)
+
+*/
+
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function (fn, t) {
+
+    return async function (...args) {
+
+        return new Promise(async (resolve, reject) => {
+
+            const setTimeId = setTimeout(() => reject("Time Limit Exceeded"), t);
+
+            try {
+                const result = await fn(...args);
+                resolve(result);
+            } catch (err) {
+                reject(err);
+            }
+
+        });
+
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
