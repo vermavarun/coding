34b8d8b1f59a25a246a5f83a6954a40973d47dc6
Varun Verma <vermavarun@outlook.com>
2024-12-17 14:04:37 +0530

    Add ReverseLinkedList solution to reverse a singly-linked list using three-pointer technique

---
+++ LeetCode/Go/0206-ReverseLinkedList.go
@@ -0,0 +1,33 @@
+/**
+
+Approach:
+1. We will use three pointers, prev, curr and temp.
+2. We will iterate through the linked list and reverse the pointers.
+3. We will return the prev pointer as the head of the reversed linked list.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func reverseList(head *ListNode) *ListNode {
+    var curr *ListNode;		// Declare the current node
+    var prev *ListNode;		// Declare the previous node
+    var temp *ListNode;		// Declare the temporary node
+
+    curr= head				// Initialize the current node
+    prev= nil				// Initialize the previous node
+    for curr != nil {		// Iterate through the linked list
+        temp= curr.Next		// Store the next node
+        curr.Next=prev		// Reverse the current node
+        prev = curr			// Move the previous node
+        curr= temp			// Move the current node
+    }
+    return prev;			// Return the reversed linked list
+}
