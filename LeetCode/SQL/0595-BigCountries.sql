83b6f84e89b0ff6a5a31da5fe4aa91f4009faa36
Varun Verma <vermavarun@outlook.com>
2024-12-18 14:53:03 +0530

    Add BigCountries SQL query to select countries with large area or population

---
+++ LeetCode/SQL/0595-BigCountries.sql
@@ -0,0 +1,6 @@
+/*
+Approach:
+1. Select name, population, area from world where area >= 3000000 or population >=25000000
+*/
+
+select name, population, area from world where area >= 3000000 or population >=25000000 
