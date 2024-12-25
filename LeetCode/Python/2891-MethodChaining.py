3cb6163db39cd444338aacf3f185623f70b2c12d
Varun Verma <vermavarun@outlook.com>
2024-12-25 15:27:13 +0530

    Add findHeavyAnimals function to filter and sort animals by weight in a DataFrame using pandas

---
+++ LeetCode/Python/2891-MethodChaining.py
@@ -0,0 +1,7 @@
+import pandas as pd
+'''
+Using the pandas library, create a function that takes a DataFrame of animals and returns only the rows where the "weight" column is greater than 100, sorted in descending order by weight, and only including the "name" column.
+query is a pandas function that filters a DataFrame based on a query string.
+'''
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    return animals.query('weight > 100').sort_values(by='weight',ascending=False)[['name']] # query filters the DataFrame to only include rows where the weight column is greater than 100, sort_values sorts the DataFrame by the weight column in descending order, and [['name']] returns a DataFrame with only the name column
