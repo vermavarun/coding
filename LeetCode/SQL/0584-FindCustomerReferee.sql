cc855e6365439da1108733cbbb83e89467e6c937
Varun Verma <vermavarun@outlook.com>
2024-12-26 18:27:25 +0530

    Add SQL query to find customers with specific referee_id conditions

---
+++ LeetCode/SQL/0584-FindCustomerReferee.sql
@@ -0,0 +1,11 @@
+/*
+https://leetcode.com/problems/find-customer-referee/solutions/6189169/simplest-solution-sql-please-upvote-by-v-3s7j/
+
+This query retrieves the name of customers from the customer table where the referee_id is either not equal to 2 or is null.
+
+select name from customer: This part specifies that we want to select the name column from the customer table.
+where referee_id != 2: This condition filters out customers whose referee_id is 2.
+or referee_id is null: This condition includes customers whose referee_id is null.
+Combining these conditions with or means that the query will return the names of customers who either do not have a referee_id of 2 or do not have a referee_id at all (i.e., it is null).
+*/
+select name from customer where referee_id != 2 or referee_id is null
