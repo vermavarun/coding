6cfaa97b56897d938c0845f3e7c92350f800b2a3
Varun Verma <vermavarun@outlook.com>
2024-12-27 13:02:42 +0530

    Add SQL query to find authors who viewed their own articles using self join

---
+++ LeetCode/SQL/1148-ArticleViewsI.sql
@@ -0,0 +1,9 @@
+-- Approach: Self Join
+-- 1. We need to find the author_id who viewed their own article.
+-- 2. We can achieve this by joining the views table with itself on author_id = viewer_id.
+-- 3. We need to select the distinct author_id as id from views where author_id = viewer_id.
+-- 4. We need to order the result by author_id.
+-- 5. Return the id.
+select distinct author_id as id from views
+    where author_id = viewer_id
+    order by author_id
