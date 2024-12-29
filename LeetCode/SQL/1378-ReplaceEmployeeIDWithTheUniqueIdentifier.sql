b79f77f4e688d0130053a863bbc88760f9698cb2
Varun Verma <vermavarun@outlook.com>
2024-12-29 14:15:49 +0530

    Add SQL query to replace EmployeeID with unique identifier using join

---
+++ LeetCode/SQL/1378-ReplaceEmployeeIDWithTheUniqueIdentifier.sql
@@ -0,0 +1,21 @@
+-- Approach: Join the two tables on the basis of id
+-- 1. We need to join the Employees and EmployeeUNI tables on the basis of the id column.
+-- 2. We can achieve this by using the join keyword to join the two tables.
+-- 3. We need to specify the columns that we want to select from the tables.
+-- 4. We need to specify the condition on which we want to join the tables.
+-- 5. Return the unique_id and name columns.
+
+select
+eu.unique_id as unique_id, e.name as name
+from Employees e
+left join EmployeeUNI eu
+on e.id = eu.id
+
+-- or
+
+-- SELECT
+--   EmployeeUNI.unique_id,
+--   Employees.name
+-- FROM Employees
+-- LEFT JOIN EmployeeUNI
+--   USING (id);
