12386a152a83df13fb21c30b5d455b4aaf68bd7d
Varun Verma <vermavarun@outlook.com>
2025-01-07 15:33:20 +0530

    Add SQL query to select non-boring movies with odd IDs and order by rating

---
+++ LeetCode/SQL/0620-NotBoringMovies.sql
@@ -0,0 +1,9 @@
+-- Approach: Select the movies which are not boring and have odd id
+-- 1. We need to select the id, movie, description, and rating from the cinema table.
+-- 2. We need to select the movies which are not boring and have an odd id.
+-- 3. We need to order the result by rating in descending order.
+-- 4. Return the id, movie, description, and rating.
+select id, movie, description, rating
+from cinema
+where description != 'boring' and id % 2 != 0
+order by rating desc;
