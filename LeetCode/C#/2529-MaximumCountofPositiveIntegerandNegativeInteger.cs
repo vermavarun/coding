3a18d775de68c1306bd05cadd7ad1d5a2646c476
Varun Verma <vermavarun@outlook.com>
2025-03-12 14:06:56 +0530

    Add implementation for counting maximum positive and negative integers in an array

---
+++ LeetCode/C#/2529-MaximumCountofPositiveIntegerandNegativeInteger.cs
@@ -0,0 +1,27 @@
+/*
+Approach: Linear Search
+1. Count the positive numbers in the array
+2. Count the negative numbers in the array
+3. Return the maximum of the positive and negative numbers count
+
+Time complexity: O(n)
+Space complexity: O(1)
+*/
+public class Solution {
+    public int MaximumCount(int[] nums) {
+       int positive_numbers_count = nums.Where(x=>x>0).Count();             // Count the positive numbers in the array
+       int negative_numbers_count = nums.Where(x=>x<0).Count();             // Count the negative numbers in the array
+       return Math.Max(positive_numbers_count,negative_numbers_count);      // Return the maximum of the positive and negative numbers count
+    }
+}
+
+/*
+Approach: Binary Search as the array is sorted
+TODO: Implement Binary Search
+
+*/
+public class Solution {
+    public int MaximumCount(int[] nums) {
+
+    }
+}
