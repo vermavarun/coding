59328c827bfca1dfc7011f3ffebe97951ff1e538
Varun Verma <vermavarun@outlook.com>
2025-03-19 21:30:17 +0530

    Add solution for Find the Difference problem in C#

---
+++ LeetCode/C#/0389-FindtheDifference.cs
@@ -0,0 +1,23 @@
+/*
+Solution: https://leetcode.com/problems/find-the-difference/solutions/6556260/simplest-solution-c-time-on-spacen-pleas-q11h/
+Approach: XOR
+1. Create a StringBuilder and append both strings to it.
+2. Initialize a char result to '\0' which is a null character.
+3. Iterate through the StringBuilder and XOR each character with result.
+4. Return the result.
+
+Time complexity: O(n)
+Space complexity: O(n)
+*/
+public class Solution {
+    public char FindTheDifference(string s, string t) {
+        StringBuilder sb = new StringBuilder(); // Initialize a StringBuilder
+        sb.Append(s);                           // Append both strings to StringBuilder
+        sb.Append(t);                           // Append both strings to StringBuilder
+        char result = '\0';                     // Initialize a char result to '\0' which is a null character
+        for(int i=0; i<sb.Length; i++) {        // Iterate through the StringBuilder
+            result ^= sb[i];                    // XOR each character with result
+        }
+        return result;                          // Return the result
+    }
+}
