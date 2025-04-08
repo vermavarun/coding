efc0c488b2ada2fdafdf5d516100d8f5ec29d1a8
Varun Verma <vermavarun@outlook.com>
2025-04-08 22:33:02 +0530

    Add solution for Minimum Number of Operations to Make Elements in Array Distinct

---
+++ LeetCode/C#/3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct.cs
@@ -0,0 +1,25 @@
+/*
+Solution: https://leetcode.com/problems/minimum-number-of-operations-to-make-elements-in-array-distinct/solutions/6630129/simplest-solution-c-time-on-spacen-pleas-29rf/
+Approach: Using HashSet and Reverse Iteration
+1) We will use a HashSet to store the unique elements of the array.
+2) We will iterate through the array in reverse order.
+3) For each element, we will check if it is already in the HashSet.
+4) If it is, we will return the number of operations needed to make the elements distinct.
+5) If it is not, we will add it to the HashSet.
+6) If we finish iterating through the array without finding any duplicates, we will return 0.
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+*/
+public class Solution {
+    public int MinimumOperations(int[] nums) {
+        HashSet<int> hs = new HashSet<int>();         // HashSet to store unique elements
+        for(int i=nums.Length - 1; i>=0; i--) {       // Reverse iterate through the array
+            if (hs.Contains(nums[i])) {               // If the element is already in the HashSet
+                return (int)Math.Ceiling((i+1)/3.0);  // Return the number of operations needed
+            }
+            hs.Add(nums[i]);                          // Add the element to the HashSet
+        }
+        return 0;                                     // If all elements are unique, return 0
+    }
+}
