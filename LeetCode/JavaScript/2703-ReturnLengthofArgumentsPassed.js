d41ea55103e51000a0b2e3cf26f26b6d42155c55
Varun Verma <vermavarun@outlook.com>
2025-11-25 19:00:41 +0530

    Add solution for "Return Length of Arguments Passed" using rest parameters

---
+++ LeetCode/JavaScript/2703-ReturnLengthofArgumentsPassed.js
@@ -0,0 +1,23 @@
+/*
+Solution: Return Length of Arguments Passed
+Approach: Use Rest Parameters and Length Property
+Tags: JavaScript, Rest Parameters, Array
+1) Use rest parameters (...args) to collect all arguments into an array.
+2) Access the length property of the arguments array.
+3) Return the count of arguments passed to the function.
+
+Time Complexity: O(1)
+Space Complexity: O(n) where n is the number of arguments
+*/
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return ([...args].length)                       // Return the number of arguments passed
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
